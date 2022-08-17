1cb5f4554c12782ce4622638aad79b30960900d2
LozanoTheStain <adslozanov@gmail.com>
2022-08-16 20:23:31 -0500

    Listo para desplegar

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,6 +1,18 @@
+import '../css/componentes.css'
+// import webpacklogo from '../aseets/img/webpack-logo.png'
+
 export const saludar = (nombre) => {
   console.log('Creando etiqueta H1')
   const h1 = document.createElement('h1')
   h1.innerText = `Hola, ${nombre}`
   document.body.append(h1)
+
+  //creación de imagen
+// console.log(webpacklogo);
+// const img = document.createElement('img')
+// img.src = webpacklogo
+// document.body.append(img)
 }
+
+
+
